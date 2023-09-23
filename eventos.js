a78dc1fcac8f6568f8c3339cb1bb9a075f695f44
THIEMAA <thiagorodriguez91@gmail.com>
2023-09-23 19:17:55 -0300

    solucion click

--- eventos.js
+++ eventos.js
@@ -1,6 +1,13 @@
-const divButtonContainer = document.getElementById('buttonContainer')
+const divButtonContainer = document.getElementById('buttonContainer');
+const saludarButton = document.getElementById('saludarButton');
 
-divButtonContainer.addEventListener('click', function(){
+
+saludarButton.addEventListener('click', function(event) {
+    event.stopPropagation(); // Evita que el evento se propague al div
+    alert('Hola! Soy el botón');
+});
+
+
+divButtonContainer.addEventListener('click', function() {
     alert('Hola! Soy el div');
-})
-    
+});
